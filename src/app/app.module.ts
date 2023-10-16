ab435526c8a29c5c3ca9200a6ca1cca3f1fad031
jonas <jonassaraivasantos@gmail.com>
2023-10-16 18:21:43 -0300

    Adicionando todos os arquivos do playstation store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { CardPricingComponent } from './components/card/card-pricing/card-pricing.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		CardComponent,
+		MenuComponent,
+		CardLabelComponent,
+		CardPricingComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
